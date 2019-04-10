3e46223a9ae62ca0a8ce9309a4f065ce2a92d0f8
Nicholas Mirchandani <nmirchandani@chapman.edu>
2019-04-09 21:42:21 -0700

    Basic Enemy Tracking + Death Console Log

---
+++ The Guy in the Chair/Assets/EnemyChase.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+public class EnemyChase : MonoBehaviour
+{
+    public GameObject tracker;
+    private GameObject player;
+    public IAstarAI ai;
+    private int lookTimer = 0;
+
+    private void Start()
+    {
+        ai = GetComponent<IAstarAI>();
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if(col.tag.Equals("Player"))
+        {
+            tracker.transform.position = col.transform.position;
+            ai.SearchPath();
+        }
+    }
+}
